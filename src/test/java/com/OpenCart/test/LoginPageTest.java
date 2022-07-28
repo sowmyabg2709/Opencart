8f656746d398a93d5f0434d877b45c7c71b48254
sowmya.bg27 <sowmya.bg@sakshath-technologies.com>
2022-07-28 17:59:01 +0530

    added

--- src/test/java/com/OpenCart/test/LoginPageTest.java
+++ src/test/java/com/OpenCart/test/LoginPageTest.java
@@ -6,10 +6,19 @@ import org.testng.annotations.Test;
 import com.OpenCart.base.BaseTest;
 import com.OpenCart.utils.Constants;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Story;
+
+@Epic("Epic:Design login page for demo app")
+@Story("US1:Develop feature with all login page scenarios")
 public class LoginPageTest extends BaseTest{
 	
 	
-	
+	@Description("login Page TitleTest")
+	@Severity(SeverityLevel.NORMAL)
 	@Test(priority=1)
 	public void logiPageTitleTest() {
 		String title= loginPage.getLoginPageTitle();
@@ -17,6 +26,8 @@ public class LoginPageTest extends BaseTest{
 		Assert.assertEquals(title,Constants.LOGIN_PAGE_TITLE);
 	}
 	
+	@Description("login Page url test")
+	@Severity(SeverityLevel.MINOR)
 	@Test(priority=2)
 	public void loginPageUrlTest() {
 		String url =loginPage.getLoginPageUrl();
@@ -24,11 +35,15 @@ public class LoginPageTest extends BaseTest{
 		Assert.assertTrue(url.contains(Constants.LOGIN_PAGE_URL));
 	}
 	
+	@Description("login Page ForgotPassword linktest")
+	@Severity(SeverityLevel.CRITICAL)
 	@Test(priority=3)
 	public void forgotPwdLinkTest() {
 		Assert.assertTrue(loginPage.isForgotPwdLinkExist());
 	}
 	
+	@Description("login Test")
+	@Severity(SeverityLevel.BLOCKER)
 	public void loginTest() {
 		loginPage.doLogin(prop.getProperty("username"),prop.getProperty("password"));
 	}
